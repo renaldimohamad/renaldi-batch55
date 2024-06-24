577289caeb55227b8011663e9a1308e25ad363d1
renaldimohamad <renaldimohamad77@gmail.com>
2024-06-25 03:36:20 +0700

    update day 14 and day 15

--- day 14/index.js
+++ day 14/index.js
@@ -1,8 +1,71 @@
 const express = require("express");
-const { sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, QueryTypes } = require("sequelize");
 
-const sequelize = new sequelize(config.development);
 const config = require("./config/config.json");
+const sequelize = new Sequelize(config.development);
+
+const testimonials = [
+  {
+    image:
+      "https://images.pexels.com/photos/7545185/pexels-photo-7545185.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    contens: "Build a foundation for the future with every piece learned",
+    author: "Antonio Elz",
+    ratting: 5,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/8929332/pexels-photo-8929332.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    contens:
+      "Flowing with harmony, letting the soul fly free along with the violin melody",
+    author: "Agatha Christie",
+    ratting: 3,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/926390/pexels-photo-926390.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    contens:
+      "Anchored in the digital world, chasing dreams one click at a time",
+    author: "Surya Elidanto",
+    ratting: 4,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/842548/pexels-photo-842548.jpeg?auto=compress&cs=tinysrgb&w=600",
+    contens: "It's time to embrace the day with a cup of warmth",
+    author: "Ethan Blackwood",
+    ratting: 4,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    contens: "Ayo jelajahi dunia, topi bulatku akan menuntun langkahku",
+    author: "Garrett Hart",
+    ratting: 3,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/10973825/pexels-photo-10973825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    contens:
+      "When the eyes are glued to the page, the soul explores the beauty of the words",
+    author: "Lucas Archer",
+    ratting: 5,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/3761513/pexels-photo-3761513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    contens:
+      "In this world, education is the key to opening the door to the future",
+    author: "Ruby Wells",
+    ratting: 2,
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/3638045/pexels-photo-3638045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    contens: "Classic never goes out of style: style with a round hat.",
+    author: "Brett Sayles",
+    ratting: 1,
+  },
+];
 
 const path = require("path");
 const { type } = require("os");
@@ -54,7 +117,7 @@ async function addBlog(req, res) {
   const date = new Date();
   const dateString = date.toISOString().slice(0, 19).replace("T", " ");
 
-  const query = `INSERT INTO "Blogs"(title, content, "createdAt", "updatedAt") VALUES ('${title}', '${content}'), '${dateString}'), '${dateString}')`;
+  const query = `INSERT INTO "Blogs" (title, content, "createdAt", "updatedAt") VALUES ('${title}', '${content}', '${dateString}', '${dateString}')`;
 
   await sequelize.query(query, { type: QueryTypes.INSERT });
 
@@ -78,9 +141,9 @@ async function updateProject(req, res) {
   const date = new Date();
   const dateString = date.toISOString().slice(0, 19).replace("T", " ");
 
-  const query = `UPDATE "Blogs" SET title='${title}', content='${content}', "createdAt"='${dateString}', "updatedAt"='${dateString} WHERE id=${id}'            `;
+  const query = `UPDATE "Blogs" SET title='${title}', content='${content}', "updatedAt"='${dateString} WHERE id=${id}'`;
   await sequelize.query(query, { type: QueryTypes.UPDATE });
-  res.redirect("/blog");
+  res.redirect("/myProject");
 }
 
 // DELETE
@@ -102,18 +165,20 @@ async function detailProject(req, res) {
   const query = `SELECT * FROM "Blogs" WHERE id='${id}'`;
   const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
 
-  console.log("check :", detail);
   res.render("detailProject", { detail: obj[0] });
-}
-
-function detailProject(req, res) {
-  res.render("detailProject");
 }
 
 // TESTIMONIAL
 
 function testimonial(req, res) {
-  res.render("testimonial");
+  const rating = parseInt(req.query.rating);
+  let filteredTestimonials = testimonials;
+
+  if (rating) {
+    filteredTestimonials = testimonials.filter((t) => t.ratting === rating);
+  }
+
+  res.render("testimonial", { testimonials: filteredTestimonials });
 }
 
 // CONTACT
