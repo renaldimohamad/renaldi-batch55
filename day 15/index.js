25165491661b9e9484e1b80bc677b01fe1bd16f1
renaldimohamad <renaldimohamad77@gmail.com>
2024-06-26 18:47:44 +0700

    update day 15 register and login

--- day 15/index.js
+++ day 15/index.js
@@ -3,6 +3,9 @@ const { Sequelize, QueryTypes } = require("sequelize");
 
 const config = require("./config/config.json");
 const sequelize = new Sequelize(config.development);
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
 
 const testimonials = [
   {
@@ -69,6 +72,9 @@ const testimonials = [
 
 const path = require("path");
 const { type } = require("os");
+const { error } = require("console");
+const { hasSubscribers } = require("diagnostics_channel");
+const { hash } = require("crypto");
 const app = express();
 const port = 5000;
 
@@ -77,6 +83,21 @@ app.set("views", path.join(__dirname, "src/views"));
 
 app.use("/assets", express.static(path.join(__dirname, "src/assets")));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(flash());
+app.use(
+  session({
+    name: "data",
+    secret: "rahasiabgtcui",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
 app.get("/", home);
 app.get("/myProject", myProject);
 app.get("/addProject", viewproject);
@@ -90,14 +111,20 @@ app.post("/deleteProject/:id", deleteProject);
 app.get("/testimonial", testimonial);
 app.get("/contact", contact);
 
-// HOME
+app.get("/login", loginView);
+app.post("/login", login);
+app.get("/register", registerView);
+app.post("/register", register);
+app.get("/logout", logout);
 
+// HOME
 function home(req, res) {
-  res.render("index");
+  const isLogin = req.session.isLogin;
+  const user = req.session.user;
+  res.render("index", { user, isLogin });
 }
 
 // MY PROJECT
-
 async function myProject(req, res) {
   const query = `SELECT * FROM "Blogs"`;
 
@@ -110,7 +137,6 @@ function viewproject(req, res) {
 }
 
 // ADD
-
 async function addBlog(req, res) {
   const { title, content } = req.body;
 
@@ -134,7 +160,6 @@ async function editProjectView(req, res) {
 }
 
 // UPDATE PROJECT
-
 async function updateProject(req, res) {
   const { title, content, id } = req.body;
 
@@ -147,7 +172,6 @@ async function updateProject(req, res) {
 }
 
 // DELETE
-
 async function deleteProject(req, res) {
   const { id } = req.params;
 
@@ -158,19 +182,16 @@ async function deleteProject(req, res) {
 }
 
 // DETAIL PROJECT
-
 async function detailProject(req, res) {
   const { id } = req.params;
 
   const query = `SELECT * FROM "Blogs" WHERE id='${id}'`;
   const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
 
-  console.log("check :", detail);
   res.render("detailProject", { detail: obj[0] });
 }
 
 // TESTIMONIAL
-
 function testimonial(req, res) {
   const rating = parseInt(req.query.rating);
   let filteredTestimonials = testimonials;
@@ -183,10 +204,92 @@ function testimonial(req, res) {
 }
 
 // CONTACT
-
 function contact(req, res) {
   res.render("contact");
 }
+
+function loginView(req, res) {
+  res.render("login-form");
+}
+
+// LOGIN
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  const query = `SELECT * FROM "Users" WHERE email='${email}'`;
+  const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  if (!obj.length) {
+    console.error("user not found");
+    req.flash("danger", "Login Failed: Email is wrong!");
+    return res.redirect("/login");
+  }
+
+  // bandingin password
+  bcrypt.compare(password, obj[0].password, (err, resault) => {
+    if (err) {
+      req.flash("danger", "Login Failed: Internal Server Error");
+      return res.redirect("/login");
+    }
+
+    if (!resault) {
+      req.flash("danger", "Login Failed: Password is wrong!");
+      return res.redirect("/login");
+    }
+
+    req.flash("success", "Login Success!");
+    req.session.isLogin = true;
+    req.session.user = {
+      name: obj[0].name,
+      email: obj[0].email,
+    };
+
+    res.redirect("/");
+  });
+}
+
+function registerView(req, res) {
+  res.render("register-form");
+}
+
+// REGISTER
+async function register(req, res) {
+  const { name, email, password } = req.body;
+
+  const query = `SELECT * FROM "Users" WHERE email='${email}'`;
+  const obj = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  // if (obj[0].length === 1) {
+  //   req.flash("danger", "Register Failed: Email Already Use!");
+  //   return res.redirect("/register");
+  // }
+
+  bcrypt.hash(password, 10, async (err, hash) => {
+    if (err) {
+      req.flash(
+        "danger",
+        "Register Failed : password failed to be encyptionsss!"
+      );
+      return res.redirect("/register");
+    }
+
+    const query = `INSERT INTO "Users"(name, email, password) VALUES ('${name}','${email}','${hash}')`;
+    await sequelize.query(query, { type: QueryTypes.INSERT });
+    req.flash("success", "Register Success!");
+    res.redirect("/login");
+  });
+}
+
+// LOGOUT
+function logout(req, res) {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/login");
+  });
+}
+
 app.listen(port, function () {
   console.log(`Server berjalan di port ${port}`);
 });
